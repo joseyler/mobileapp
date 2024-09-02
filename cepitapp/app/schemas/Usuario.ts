28fb55498743cb74d381f0a1236cee3e531f5734
Martin <martinfernandodelgado23@gmail.com>
2024-09-02 20:58:45 -0300

    Esquema User

---
+++ cepitapp/app/schemas/Usuario.ts
@@ -0,0 +1,19 @@
+import Realm, { BSON, ObjectSchema } from "realm";
+
+export class User extends Realm.Object<User> {
+    _id!: BSON.ObjectId;
+    username!: string;
+    email!:string;
+    role!: string;
+
+    static schema: ObjectSchema = {
+      name: 'User',
+      properties: {
+        _id: 'objectId',
+        username: {type: 'string', indexed: 'full-text'},
+        email: {type: 'string', indexed: 'full-text'},
+        rol: {type: 'string', indexed: 'full-text'},
+      },
+      primaryKey: '_id',
+    };
+  }
